8799172a8f37a5adc046b662b55556df82a2e435
km201802 <36888473+km201802@users.noreply.github.com>
2019-10-15 13:33:25 -0400

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,65 +1,34 @@
 function time() {
-    var fecha = new Date();
-    var milli = fecha.getMilliseconds();
-    var sec = fecha.getSeconds(); //obtemos los segundos
-    var sev; //variable para la condicion 00
-    if (sec < 10) {
-        sev = "0" + sec; // agregamos un 0 cuando var<10
-    } else {
-        sev = sec; //si no lo es dejamos el mismo valor
-    } //para agregar un 0 cuando es menor que 10 ejem:9 a 09
-    var min = fecha.getMinutes(); //obtenemos los minutos
-    var miv;
-    if (min < 10) {
-        miv = "0" + min;
-    } else {
-        miv = min;
-    }
-    var hora = fecha.getHours();
-    var horav;
-    if (hora > 12) {
-        hora = hora - 12;
-    }
-    if (hora < 10) {
-        horav = "0" + hora;
-    } else {
-        horav = hora;
-    }
-    var millisec = milli / 100;
-    var msec = milli / 1000 * 250;
-    var colored = sec / 60 * 250;
-    //alert(msec);
-
-    document.getElementById("demo").innerHTML = horav + ":" + miv + ":" + sev + ":" + Math.floor(millisec);
-    document.getElementById("demo").style.color = "rgb(" + msec + ", " + colored + "," + msec + ")";
+    var date = new Date();
+    var millis = Math.floor(date.getMilliseconds()/100);
+    var seconds = date.getSeconds();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    document.getElementById("demo").innerHTML = hours + ":" + minutes + ":" + seconds + ":" + Math.floor(millis);
     //document.getElementById("milli").innerHTML = Math.floor(r);
 }
 
 function fechadma() {
-    var fecha = new Date();
-    var dia = fecha.getDay() - 1; //dia
-    var dia30 = fecha.getDate(); //dia del mes
-    var mes = fecha.getMonth() + 1; //mes
-    var year = fecha.getFullYear();
-
-
-    var meses = ["Enero", "Febrero", "Marzo", "abril", "mayo", "junio", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
-    var dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]; //arreglos de dias de semana
-    document.getElementById("date").innerHTML = dias[dia] + ", " + dia30 + " de " + meses[mes] + " de " + year;
-
-}
-
-function fechain() {
-    var fecha = new Date();
-    var dia = fecha.getDay() - 1; //dia
-    var dia30 = fecha.getDate(); //dia del mes
-    var mes = fecha.getMonth() + 1; //mes
-    var year = fecha.getFullYear();
+    var date = new Date();
+    var days = date.getDay() - 1;
+    var monthDay = date.getDate();
+    var months = date.getMonth() + 1;
+    var years = date.getFullYear();
 
     var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    document.getElementById("date").innerHTML = days[dia] + ", " + month[mes] + " " + dia30 + ", " + year;
+    var names = {
+       meses: ["Enero", "Febrero", "Marzo", "abril", "mayo", "junio", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
+       dias: ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"],
+       month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+       days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    }
+    document.getElementById("date").innerHTML = name.days[days] + ", " +  + " " + dia30 + ", " + year;
+    document.getElementById("date").innerHTML = names.dias[days] + ", " + monthDay + " de " + names.meses[months] + " de " + years;
+
 }
+
+
 time();
 setInterval(time, 50);
 fechadma();
