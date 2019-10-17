00b07c1221d551dc955f7fa4776a3ce7869a0789
km201802 <36888473+km201802@users.noreply.github.com>
2019-10-17 08:07:00 -0400

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -10,21 +10,18 @@ function time() {
 
 function fechadma() {
     var date = new Date();
-    var days = date.getDay() - 1;
+    var day = date.getDay() - 1;
     var monthDay = date.getDate();
-    var months = date.getMonth() + 1;
+    var month = date.getMonth();
     var years = date.getFullYear();
-
-    var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     var names = {
-       meses: ["Enero", "Febrero", "Marzo", "abril", "mayo", "junio", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
+       meses: ["Enero", "Febrero", "Marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
        dias: ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"],
-       month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-       days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+       months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+       days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
     }
-    document.getElementById("date").innerHTML = name.days[days] + ", " +  + " " + dia30 + ", " + year;
-    document.getElementById("date").innerHTML = names.dias[days] + ", " + monthDay + " de " + names.meses[months] + " de " + years;
+    document.getElementById("date").innerHTML = names.days[day]+", "+names.months[month]+" "+monthDay+", "+years;
+    //document.getElementById("date").innerHTML = names.dias[day] + ", " + monthDay + " de " + names.meses[month] + " de " + years;
 
 }
 
